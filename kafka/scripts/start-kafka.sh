c5e2cb6c08fba2ba3e449f377cbab6453bbb8b38
Will Wang Z <will.z.wang@ericsson.com>
2016-08-10 18:27:27 +0800

    US#17874: Remove zookeeper from dockerfile
    Change-Id: Iec72f959e3f18f2a3a88069f1a4a5436b6ae9943

--- kafka/scripts/start-kafka.sh
+++ kafka/scripts/start-kafka.sh
@@ -3,7 +3,7 @@
 # Optional ENV variables:
 # * ADVERTISED_HOST: the external ip for the container, e.g. `docker-machine ip \`docker-machine active\``
 # * ADVERTISED_PORT: the external port for Kafka, e.g. 9092
-# * ZK_CHROOT: the zookeeper chroot that's used by Kafka (without / prefix), e.g. "kafka"
+# * ZK_CONNECT: the zookeeper connect string for Kafka, e.g. 127.0.0.1:2181
 # * LOG_RETENTION_HOURS: the minimum age of a log file in hours to be eligible for deletion (default is 168, for 1 week)
 # * LOG_RETENTION_BYTES: configure the size at which segments are pruned from the log, (default is 1073741824, for 1GB)
 # * NUM_PARTITIONS: configure the default number of log partitions per topic
@@ -24,21 +24,11 @@ if [ ! -z "$ADVERTISED_PORT" ]; then
     sed -r -i "s/#(advertised.port)=(.*)/\1=$ADVERTISED_PORT/g" $KAFKA_HOME/config/server.properties
 fi
 
-# Set the zookeeper chroot
-if [ ! -z "$ZK_CHROOT" ]; then
-    # wait for zookeeper to start up
-    until /usr/share/zookeeper/bin/zkServer.sh status; do
-      sleep 0.1
-    done
-
-    # create the chroot node
-    echo "create /$ZK_CHROOT \"\"" | /usr/share/zookeeper/bin/zkCli.sh || {
-        echo "can't create chroot in zookeeper, exit"
-        exit 1
-    }
-
+# Set the zookeeper connect string
+if [ ! -z "$ZK_CONNECT" ]; then
+    echo "zookeeper connect string: $ZK_CONNECT"
     # configure kafka
-    sed -r -i "s/(zookeeper.connect)=(.*)/\1=localhost:2181\/$ZK_CHROOT/g" $KAFKA_HOME/config/server.properties
+    sed -r -i "s/(zookeeper.connect)=(.*)/\1=localhost:2181\/$ZK_CONNECT/g" $KAFKA_HOME/config/server.properties
 fi
 
 # Allow specification of log retention policies
