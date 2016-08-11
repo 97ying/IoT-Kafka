44d372f4112b3cd97643c2410a284dad299cbae4
Will Wang Z <will.z.wang@ericsson.com>
2016-08-11 15:25:03 +0800

    US#17874: Update create-iot-topic.sh to include KAFKA_TOPIC for setting the default topic
    Change-Id: If38c7d84a1e1c75b5b591fdc54759c136f14f6df

--- kafka/scripts/create-iot-topic.sh
+++ kafka/scripts/create-iot-topic.sh
@@ -1,10 +1,28 @@
 #!/bin/sh
 
-# Wait for zookeeper and kafka startup
-echo "zookeeper connect string: $ZK_CONNECT"
-echo "sleep 30 seconds, wait for zookeeper and kafka startup"
-/bin/sleep 30
+# Set the zookeeper connect string
+zk_connect="localhost:2181"
+if [ ! -z "$ZK_CONNECT" ]; then
+    zk_connect=$ZK_CONNECT
+fi
+echo "zookeeper connect string: $zk_connect"
 
-# Create topic iot-devices-topic
-echo "generate iot-devices-topic"
-$KAFKA_HOME/bin/kafka-topics.sh --create --zookeeper $ZK_CONNECT --replication-factor 1 --partitions 1 --topic iot-devices-topic
+# Set the topic to be created
+kafka_topic="iot-devices-topic"
+if [ ! -z "$KAFKA_TOPIC" ]; then
+    kafka_topic=$KAFKA_TOPIC
+fi
+echo "kafka topic: $kafka_topic"
+
+# Wait for zookeeper and kafka startup
+echo "sleep 10 seconds, wait for zookeeper and kafka startup"
+/bin/sleep 10
+
+# Create topic
+echo "generate topic: $kafka_topic"
+response=$(bin/kafka-topics.sh --create --zookeeper $ZK_CONNECT --replication-factor 1 --partitions 1 --topic $kafka_topic)
+
+if [[ $response == *"already exists"* ]] || [[ $response == *"Created topic"* ]]
+then
+  exit 0;
+fi
